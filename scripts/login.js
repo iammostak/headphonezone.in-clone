a260d6f61f88d764e454645f9981f241ecb48e4b
Mehul <mehul19356@gmail.com>
2022-06-14 23:28:54 +0530

    added login and signup functionality

---
+++ scripts/login.js
@@ -0,0 +1,30 @@
+let form = document.getElementById("form");
+form.addEventListener("submit", checkDetails);
+
+let user = JSON.parse(localStorage.getItem("userCredentials")) || [];
+
+function checkDetails(){
+    event.preventDefault();
+
+    let mail = form.mail.value;
+    let password = form.passkey.value;
+
+    form.mail.value="";
+    form.passkey.value="";
+
+    let count=0;
+    for(let i=0; i<user.length; i++)
+    {
+        if(user[i].email===mail && user[i].password===password)
+        {
+            alert("Login Successful");
+        }
+        else{
+            count++;
+        }
+    }
+    if(count===user.length)
+    {
+        alert("Please check entered credentials");
+    }
+}
---
+++ scripts/signup.js
@@ -0,0 +1,35 @@
+let form = document.getElementById("form");
+form.addEventListener("submit", saveUserData);
+
+let dataFrame = JSON.parse(localStorage.getItem("userCredentials")) || [];
+
+function saveUserData(){
+    event.preventDefault();
+
+    class Create{
+        constructor(f_name, l_name, email, password){
+            this.first_name = f_name;
+            this.last_name = l_name;
+            this.email = email;
+            this.password = password;
+        }
+    }
+
+    if(form.first_name.value==="" || form.last_name.value==="" || form.email.value==="" || form.password.value==="")
+    {
+        alert("Please Enter Complete Credentials. Sorry Account cannot be created");
+    }
+    else
+    {
+        let c1 = new Create(form.first_name.value, form.last_name.value, form.email.value, form.password.value);
+
+        dataFrame.push(c1);
+
+        localStorage.setItem("userCredentials", JSON.stringify(dataFrame));
+
+        form.first_name.value="";
+        form.last_name.value=""; 
+        form.email.value="";
+        form.password.value="";
+    }
+}
